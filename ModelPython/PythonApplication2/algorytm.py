aba294cc35e936203fcb7c60da1b9307947f4b5f
adrianmo14857 <57635925+adrianmo14857@users.noreply.github.com>
2019-11-28 18:59:54 +0100

    Add files via upload

--- ModelPython/PythonApplication2/algorytm.py
+++ ModelPython/PythonApplication2/algorytm.py
@@ -18,14 +18,15 @@ Tpco = 27;          #Temperatura wody wyplywajacej z budynku
 Fzco = 10;          #nie wiem xd
 Tzm = 70 - 2.5*(To - 6);  #strumien ogrzanej wody
 d = 0.01
-
+Tzco = [{"Tzco": 0}] #to będzie pobierał od nas moduł 6
+Tpm = [{"Tpm": 0}] #tak samo
 #adress
 ur_address = "http://127.0.0.1:5000"
-module3_address = "http://127.0.0.1:5000"
-module5_address = "https://f5a8b30a.ngrok.io"
-module61_address = "http://127.0.0.1:5000"
-module62_address = "http://127.0.0.1:5000"
-module63_address = "http://127.0.0.1:5000"
+module3_address = "http://72e859c9.ngrok.io"
+module5_address = "http://72e859c9.ngrok.io"
+module61_address = "http://72e859c9.ngrok.io"
+module62_address = "http://72e859c9.ngrok.io"
+module63_address = "http://72e859c9.ngrok.io"
 #endpoints
 
 
@@ -40,36 +41,41 @@ def Tzcof(Fzco,ro,cw,Tzco,Tpco,kw,Tpm,Mco,cwym): #zwraca Tzco[i+1]
 
 def calculate(Mm, Mco,cwym,ro,cw,kw,Fzco):
   #dwa requesty do modulu 3
-
-  '''res = requests.get(module3_address)
+  
+  res = requests.get(module3_address+"/dostawca")
   data = res.json()
-  To = data[0]['T_o']
+  To = data['dostawca'][0]['T_o']
 
-  res = requests.get(module3_address)
+  res = requests.get(module3_address+"/dostawca")
   data = res.json()
-  Tzm = data[0]['T_zm']
+  Tzm = data['dostawca'][0]['T_zm']
 
   ###########
   #jeden request do modułu 5, w tym requescie wysyłamy im wartość Tzco
-
+  res = requests.put(module5_address+"/regulation",json = {'Tzco':222})
+  data = res.json()
+  Fzm = data['regulation'][0]['Fzm']
  
 
   ##########
   #3requesty do modułu 6, do każdego budynk osobno
 
-  res = requests.get(module61_address + "/Tpco")
-  data = res.json()['Tpco']
-  Tpco1 = data[0]['Tpco']
+  res = requests.get(module61_address + "/budynek1")
+  data = res.json()
+  Tpco1 = data['budynek1'][0]['Tpco']
+  Fcob1 = data['budynek1'][0]['Fcob']
 
-  res = requests.get(module62_address + "/Tpco")
-  data = res.json()['Tpco']
-  Tpco2 = data[0]['Tpco']
+  res = requests.get(module62_address + "/budynek2")
+  data = res.json()
+  Tpco2 = data['budynek2'][0]['Tpco']
+  Fcob2 = data['budynek2'][0]['Fcob']
 
-  res = requests.get(module63_address + "/Tpco")
-  data = res.json()['Tpco']
-  Tpco3 = data[0]['Tpco']  
+  res = requests.get(module63_address + "/budynek3")
+  data = res.json()
+  Tpco1 = data['budynek3'][0]['Tpco']
+  Fcob1 = data['budynek3'][0]['Fcob']
 
-  res = requests.get(module61_address + "/Fcob")
+  '''res = requests.get(module61_address + "/Fcob")
   data = res.json()['Fcob']
   Fcob1 = data[0]['Fcob']
 
@@ -79,46 +85,41 @@ def calculate(Mm, Mco,cwym,ro,cw,kw,Fzco):
 
   res = requests.get(module63_address + "/Fcob")
   data = res.json()['Fcob']
-  Fcob3 = data[0]['Fcob']
-  Tpco = (Tpco1*Fcob1+Tpoc2*Fcob2+Tpco3*Fcob3)/(Fcob1+Fcob2+Fcob3) #średnia ważona '''
+  Fcob3 = data[0]['Fcob'] '''
 
-  res = requests.put(module5_address+"/regulation",json = {'Tzco':Tzco})
-  data = res.json()['Fzm']
-  Fzm = data[0]['Fzm']
+  #Tpco = (Tpco1*Fcob1+Tpoc2*Fcob2+Tpco3*Fcob3)/(Fcob1+Fcob2+Fcob3) #średnia ważona '''
 
-  Tpm = Tpmf(Fzm, ro, cw, Tzm, Tpm, Tzco, Mm, cwym)
-  Tzco = Tzcof(Fzco, ro, cw, Tzco, Tpco, kw, Tpm, Mco, cwym)
+  for i in range (1,10):
+    Tpm[0]['Tpm'] = Tpmf(Fzm, ro, cw, Tzm, Tpm[0]['Tpm'], Tzco[0]['Tzco'], Mm, cwym)
+    Tzco[0]['Tzco'] = Tzcof(Fzco, ro, cw, Tzco[0]['Tzco'], Tpco, kw, Tpm[0]['Tpm'], Mco, cwym)
   
-#while(1):
-res = requests.get(ur_address + "/impuls")
-data = res.json()['impuls']
-impuls = data[0]['impuls']
-impuls = 1
-print(impuls)
-if(impuls == 1):
-  print("test1")
-  for i in range(0,100):
-    res = requests.get(ur_address + "/Tzco")
-    data = res.json()['Tzco']
-    Tzco = data[0]['Tzco']
+while(1):
+  res = requests.get(ur_address + "/impuls")
+  data = res.json()['impuls']
+  impuls = data[0]['impuls']
+  print(impuls)
+  if(impuls == 1):
+    print("test1")
+    for i in range(0,5):
+      res = requests.get(ur_address + "/Tzco")
+      data = res.json()
+      Tzco = data['Tzco']
 
-    res = requests.get(ur_address + "/Tpm")
-    data = res.json()['Tpm']
-    Tpm = data[0]['Tpm']
+      res = requests.get(ur_address + "/Tpm")
+      data = res.json()
+      Tpm = data['Tpm']
   
-    print(data[0]['Tpm'])
+      
+      calculate(Mm, Mco,cwym,ro,cw,kw,Fzco)
+      
+      #{'Fzm': 22.22222222222222, 'Um': 1}
+      #Tzco = Tzcof(Fzco, ro, cw, Tzco, Tpco, kw, Tpm, Mco, cwym)
+      #Tpm = Tpmf(Fzm, ro, cw, Tzm, Tpm, Tzco, Mm, cwym)
+      print(Tpm)
+      print(Tzco)
+      res = requests.put(ur_address + "/Tpm",json={'Tpm':Tpm[0]['Tpm']})
+      res1 = requests.put(ur_address + "/Tzco",json={'Tzco':Tzco[0]['Tzco']})
 
-    print(Tpm)
-        #calculate(Mm, Mco,cwym,ro,cw,kw,Fzco)
-    res = requests.put(module5_address+"/regulation",json = {'Tzco':Tzco})
-    data = res.json()['Fzm']
-    Fzm = data[0]['Fzm']
-    #{'Fzm': 22.22222222222222, 'Um': 1}
-    Tzco = Tzcof(Fzco, ro, cw, Tzco, Tpco, kw, Tpm, Mco, cwym)
-    Tpm = Tpmf(Fzm, ro, cw, Tzm, Tpm, Tzco, Mm, cwym)
-    
-    print(Tzco)
-    res = requests.put(ur_address + "/Tpm",json={'Tpm':Tpm})
-    res1 = requests.put(ur_address + "/Tzco",json={'Tzco':Tzco})
+
     res2 = requests.put(ur_address + "/impuls",json={'impuls':0})
 
--- ModelPython/PythonApplication2/app.py
+++ ModelPython/PythonApplication2/app.py
@@ -1,98 +1,14 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-import time
+import requests
 app = Flask(__name__)
 
-#Constants
-Mm = 3000;          #Masa wymiennika po stronie pierwotnej
-Mco = 3000;         #Masa wymiennika po stronie wtornej
-
-cwym = 2700;        #zastepcze cieplo wlasciwe wymiennika
-ro = 1000;          #gestosc wody
-cw = 4200;          #cieplo wlasciwe wody
-kw = 250000;        #wspolczynnik przenikania ciepla
-Fzco = 40;
-
-#Variables
-Fzm = 1;          #Strumien ogrzanej wody od 0 do ok. 22
-To = -5;          #Temperatura zewnetrzna
-Tpco = 27;          #Temperatura wody wyplywajacej z budynku
-Fzco = 10;          #nie wiem xd
-Tzm = 70 - 2.5*(To - 6);  #strumien ogrzanej wody
-d = 0.01
-#adress
-module3_address = "http://127.0.0.1:5000"
-module5_address = "http://127.0.0.1:5000"
-module61_address = "http://127.0.0.1:5000"
-module62_address = "http://127.0.0.1:5000"
-module63_address = "http://127.0.0.1:5000"
-#endpoints
 start = [{}]  #metoda put do modułu czasowego 
-timeS  = [{"speed":0,"symTime" : "2019-11-18+18:40:15" }]  #metoda put do modułu czasowego
-Tzco = [{"Tzco": 0,"To":1}] #to będzie pobierał od nas moduł 6
+time  = [{"speed":0,"symTime" : "2019-11-18+18:40:15" }]  #metoda put do modułu czasowego
+Tzco = [{"Tzco": 0}] #to będzie pobierał od nas moduł 6
 Tpm = [{"Tpm": 0}] #tak samo
-
-#differentials
-def Tpmf(Fzm,ro,cw,Tzm,Tpm,Tzco,Mm,cwym): #zwraca Tpm[i+1]
-  difference = ((Fzm*ro*cw*(Tzm - Tpm)) - (kw * (Tpm - Tzco)) )/ (Mm*cwym)
-  return (difference*d)+Tpm
-
-def Tzcof(Fzco,ro,cw,Tzco,Tpco,kw,Tpm,Mco,cwym): #zwraca Tzco[i+1]
-  difference = ((-Fzco) * ro*cw*(Tzco - Tpco) + kw * (Tpm - Tzco))/(Mco*cwym)
-  return (difference*d)+Tzco;
-
-def calculate(Mm, Mco,cwym,ro,cw,kw,Fzco):
-  '''#dwa requesty do modulu 3
-
-  res = requests.get(module3_address)
-  data = res.json()
-  To = data[0]['T_o']
-
-  res = requests.get(module3_address)
-  data = res.json()
-  Tzm = data[0]['T_zm']
-
-  ###########
-  #jeden request do modułu 5, w tym requescie wysyłamy im wartość Tzco
-
-  res = requests.get(module5_address,params = Tzco)
-  data = res.json()['Fzm']
-  Fzm = data[0]['Fzm']
-
-  ##########
-  #3requesty do modułu 6, do każdego budynk osobno
-
-  res = requests.get(module61_address + "/Tpco")
-  data = res.json()['Tpco']
-  Tpco1 = data[0]['Tpco']
-
-  res = requests.get(module62_address + "/Tpco")
-  data = res.json()['Tpco']
-  Tpco2 = data[0]['Tpco']
-
-  res = requests.get(module63_address + "/Tpco")
-  data = res.json()['Tpco']
-  Tpco3 = data[0]['Tpco']  
-
-  res = requests.get(module61_address + "/Fcob")
-  data = res.json()['Fcob']
-  Fcob1 = data[0]['Fcob']
-
-  res = requests.get(module62_address + "/Fcob")
-  data = res.json()['Fcob']
-  Fcob2 = data[0]['Fcob']
-
-  res = requests.get(module63_address + "/Fcob")
-  data = res.json()['Fcob']
-  Fcob3 = data[0]['Fcob']
-  Tpco = (Tpco1*Fcob1+Tpoc2*Fcob2+Tpco3*Fcob3)/(Fcob1+Fcob2+Fcob3) #średnia ważona '''
-
-  for i in range(0,5):
-    Tpm[0]['Tpm'] = Tpmf(Fzm, ro, cw, Tzm, Tpm[0]['Tpm'], Tzco[0]['Tzco'], Mm, cwym)
-    Tzco[0]['Tzco'] = Tzcof(Fzco, ro, cw, Tzco[0]['Tzco'], Tpco, kw, Tpm[0]['Tpm'], Mco, cwym)
-  
-
+impuls = [{"impuls": 0}]
 #methods			 
 @app.route('/Tzco', methods=['GET'])
 def getTzco():
@@ -102,20 +18,45 @@ def getTzco():
 def getTpm():
     return jsonify({'Tpm' : Tpm})
 
+@app.route('/Tpm', methods=['PUT'])
+def editTpm():
+    new_Tpm = request.get_json()
+    Tpm[0] = new_Tpm    
+    qs = request.get_json()
+    return jsonify({}) 
+
+@app.route('/Tzco', methods=['PUT'])
+def editTzco():
+    new_Tzco = request.get_json()
+    Tzco[0] = new_Tzco    
+    qs = request.get_json()
+    return jsonify({}) 
+
 @app.route('/start', methods=['GET'])
 def getStart():
     return jsonify({'start' : start})
 
-@app.route('/timeS', methods=['GET'])
+@app.route('/time', methods=['GET'])
 def getTime():
-    return jsonify({'timeS' : timeS})
+    return jsonify({'time' : time})
+
+@app.route('/impuls', methods=['GET'])
+def getimpuls():
+    return jsonify({'impuls' : impuls})
+
+@app.route('/impuls', methods=['PUT'])
+def editimpuls():
+    new_impuls = request.get_json()
+    impuls[0] = new_impuls    
+    qs = request.get_json()
+    return jsonify({})  
 
 @app.route('/time', methods=['PUT'])
 def editTime():
     new_time = request.get_json()
     time[0] = new_time    
     qs = request.get_json()
-    calculate(Mm, Mco,cwym,ro,cw,kw,Fzco)
+    impuls[0] = {"impuls": 1}
     return jsonify({})       
        
 @app.route('/start', methods=['PUT'])
@@ -123,9 +64,10 @@ def editStart():
     new_start = request.get_json()
     start[0] = new_start    
     qs = request.get_json()
-    calculate(Mm, Mco,cwym,ro,cw,kw,Fzco)
+    impuls[0] = {"impuls": 1}
     return jsonify({})
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
